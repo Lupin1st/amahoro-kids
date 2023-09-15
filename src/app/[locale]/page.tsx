77ed068bd90f87c5d10ff76ad48c348b7f0195f6
medunova <medunova.anet@seznam.cz>
2023-09-15 23:30:30 +0200

    Features/homepage (#5)
    * Blocks
    
    * labels
    
    ---------
    
    Co-authored-by: ext_medunova <ext_medunova@keba.com>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,14 +2,16 @@
 
 import { useTranslations } from 'next-intl';
 import HomeIntro from "@/components/Sections/HomeIntro";
+import HomeBlocks from '@/components/Sections/HomeBlocks';
 
 
 export default function IndexPage() {
   const t = useTranslations('IndexPage');
 
   return (
-  <>
+    <>
       <HomeIntro />
-  </>
-);
+      <HomeBlocks />
+    </>
+  );
 }
