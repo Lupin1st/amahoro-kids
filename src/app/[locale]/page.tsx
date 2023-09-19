2aaa2d90f115bb211fae28228e0ed95ede041c30
ext_medunova <ext_medunova@keba.com>
2023-09-19 23:08:07 +0200

    Slider main page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,30 +5,94 @@ import { useTranslations } from 'next-intl';
 import HomeIntro from '@/components/Sections/HomeIntro';
 import HomeBlocks from '@/components/Sections/HomeBlocks';
 import Carousel from 'react-material-ui-carousel';
+import { Button } from '@mui/material';
+import Link from 'next-intl/link';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import GroupIcon from '@mui/icons-material/Group';
+import AgricultureIcon from '@mui/icons-material/Agriculture';
 
 export default function IndexPage() {
+  const t = useTranslations('IndexPage');
+
   return (
     <>
       <div className="w-full flex flex-col items-center space-y-5">
-      <Carousel className="w-full" 
-          height={400}
-          fullHeightHover={true}
-          animation="fade"
-          interval={7000}
-          duration={2000}
-          navButtonsAlwaysVisible={true}
-          indicators={true}
-          sx={{
-            backgroundColor: 'rgba(33, 123, 179, 0.1)',
-          }}
-        >
-          <img src="/sliders/project_1.jpg" style={{marginLeft:"auto",marginRight:"auto"}} />
-          <img src="/sliders/project_2.jpg" style={{marginLeft:"auto",marginRight:"auto"}} />
-          <img src="/sliders/project_3.jpg" style={{marginLeft:"auto",marginRight:"auto"}} />
-          <img src="/sliders/project_4.jpg" style={{marginLeft:"auto",marginRight:"auto"}} />
-          <img src="/sliders/project_5.jpg" style={{marginLeft:"auto",marginRight:"auto"}} />
+        <div className="w-full">
+          <Carousel
+            height={500}
+            fullHeightHover={true}
+            animation="fade"
+            interval={7000}
+            duration={2000}
+            navButtonsAlwaysVisible={true}
+            indicators={true}
+            sx={{
+              backgroundColor: 'rgba(33, 123, 179, 0.1)',
+            }}
+          >
+            <div>
+              {/*<TODO>Create template for this</TODO> */}
+              
+              {/*<TODO>Mobile view </TODO> */}
+              <div className='w-full'>
+                <img src="/sliders/project_1.jpg" style={{ marginLeft: "auto", marginRight: "auto" }} />
+              </div>
 
-        </Carousel>
+              <div className='absolute bg-blue-200 bg-opacity-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+                <h1>Organic farm</h1>
+                <p>Information about organic farm</p>
+              </div>
+              <div className='absolute bg-blue-200 bg-opacity-50 top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+                <Button
+                  component={Link}
+                  href="/projects"
+                  variant="contained"
+                  startIcon={<AgricultureIcon/>}
+                >
+                  {t('DiscoverMore')}
+                </Button>
+              </div>
+            </div>
+            <div>
+              <div className='w-full'>
+                <img src="/sliders/project_3.jpg" style={{ marginLeft: "auto", marginRight: "auto" }} />
+              </div>
+              <div className='absolute bg-blue-200 bg-opacity-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+                <h1>Amahoro Children</h1>
+                <p>Partner children program</p>
+              </div>
+              <div className='absolute bg-blue-200 bg-opacity-50 top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+                <Button
+                  component={Link}
+                  href="/help"
+                  variant="contained"
+                  startIcon={<VolunteerActivismIcon />}
+                >
+                  {t('HelpNow')}
+                </Button>
+              </div>
+            </div>
+            <div>
+              <div className='w-full'>
+                <img src="/sliders/project_5.jpg" style={{ marginLeft: "auto", marginRight: "auto" }} />
+              </div>
+              <div className='absolute bg-blue-200 bg-opacity-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+                <h1>Label image 5</h1>
+                <p>Information about this slide 5</p>
+              </div>
+              <div className='absolute bg-blue-200 bg-opacity-50 top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+                <Button
+                  component={Link}
+                  href="/about"
+                  variant="contained"
+                  startIcon={<GroupIcon/>}
+                >
+                  {t('AboutUs')}
+                </Button>
+            </div>
+            </div>
+          </Carousel>
+        </div>
 
         <HomeBlocks />
         <Carousel
